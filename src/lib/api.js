b542e514d1d189a2d680c28b025289c141cafe12
yejin <johnyejin@kakao.com>
2020-06-24 18:10:01 +0900

    fetch problem

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,10 +1,9 @@
 const api = async (page) => {
-  console.log('api.js---page', page)
   if(isNaN(page)) {
     return [];
   }
 
-  const res = await fetch(`http://node-hnapi.herokuapp.com/news?page=${page}`);
+  const res = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
   const data = await res.json();
   
   return data;
