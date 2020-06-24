5d8d633282bedeb8d82fe05bb7175feacf0c165b
yejin <johnyejin@kakao.com>
2020-06-24 17:43:19 +0900

    Updates

---
+++ precache-manifest.f90c9b8c327ec55b9690fbbb2642e898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b34179ddca336c63013955af4c3a19e",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "39a9365bd914f0be8b76",
+    "url": "/hackernews/static/css/main.28b214e7.chunk.css"
+  },
+  {
+    "revision": "970b353a222cd2ee5f8a",
+    "url": "/hackernews/static/js/2.e423c7b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackernews/static/js/2.e423c7b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a9365bd914f0be8b76",
+    "url": "/hackernews/static/js/main.2fff0837.chunk.js"
+  },
+  {
+    "revision": "3c88cad4107bc760ff70",
+    "url": "/hackernews/static/js/runtime-main.79177eaa.js"
+  }
+]);
